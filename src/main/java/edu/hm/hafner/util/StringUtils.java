73690ccb10c54b7c666e912d04d304f0e2a9010f
Marvin Schuetz <marvin-spiky@gmx.net>
2013-04-17 12:15:34 +0200

    test plus methoden abgabe 1

--- src/main/java/edu/hm/hafner/util/StringUtils.java
+++ src/main/java/edu/hm/hafner/util/StringUtils.java
@@ -2,6 +2,7 @@
 
 package edu.hm.hafner.util;
 
+
 /**
  * Several useful utility methods that work on {@link String} instances.
  *
@@ -10,6 +11,8 @@ package edu.hm.hafner.util;
  * @author habast
  */
 public final class StringUtils {
+
+
     /**
      * Prüft, ob der übergebene String leer ist, d.h. kein Zeichen enthält.
      *
@@ -29,5 +32,91 @@ public final class StringUtils {
      */
     private StringUtils() {
         // prevents instantiation
+    }
+
+    /**
+     * Testet ob ein String nur aus Blanks besteht
+     * @param toTest
+     * @return true falls der String nur aus Blanks besteht sonst false
+     */
+    public static boolean isBlank(final String toTest) {
+        if(toTest == null) {
+            return true;
+        }
+        else {
+            for(int i=0;i<toTest.length();i++){
+                if(toTest.charAt(i) != ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     *Verbindet Strings zu einem neuen.
+     *@author Marvin Schütz
+     *@param elements
+     *@return result neuer string
+     */
+    public static String join(final String... elements) {
+        String result ="";
+        if(elements.length==0) {
+            throw new IllegalArgumentException();
+        }
+        else{
+            for(int i = 0; i<elements.length; i++){
+                if(elements[i]!=null) {
+                    result+=elements[i];
+                }
+                else {
+                    result+="(null)";
+                }
+                if(i<elements.length-1){
+                    result+=",";
+                }
+            }
+        }
+        return result;
+    }
+    /**
+     * Prüft eine 10 stellige ISBN auf Gültigkeit.
+     * @author Marvin Schütz
+     * @param isbnEingabe
+     * @return false falls die Eingabe ungültig ist ansonsten true
+     */
+    public static boolean isValidIsbn10(final String isbnEingabe){
+        int sum = 0;
+
+        //abfrage ob null und ob die länge in Ordnung ist
+        if(isbnEingabe == null || isbnEingabe.length()!=13) {
+            return false;
+        }
+        //entfärnt die Formatierungszeichen
+        String[] tmp = isbnEingabe.split("[ \\-]");
+        String isbn="";
+        for(int i=0;i<4;i++){
+            isbn+=tmp[i];
+        }
+        //nochmal testen auf die richtige länge falls zuviele fotmatierungszeichen enthalten waren
+        if(isbn.length()!=10) {
+            return false;
+        }
+        for(int i=0 ; i<=8;i++){
+           int digit = Character.getNumericValue(isbn.charAt(i));
+           //prüft ob dass zeichen eine zahl ist
+           if(!(-1<digit&&digit<10)) {
+            return false;
+           }
+        else {
+            sum+=(i+1)*digit;
+        }
+        }
+        sum =sum %11;
+        //prüf ziffer wird getestet
+        if(sum != Character.getNumericValue(isbn.charAt(9))) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/edu/hm/hafner/util/StringUtilsTest.java
+++ src/test/java/edu/hm/hafner/util/StringUtilsTest.java
@@ -28,5 +28,67 @@ public class StringUtilsTest {
         assertFalse("Der String sollte nicht als empty erkannt werden", StringUtils.isEmpty("\t"));
         assertFalse("Der String sollte nicht als empty erkannt werden", StringUtils.isEmpty("Hallo"));
     }
+    /**
+     * testet die methode isBlank auf true Ausgabe.
+     * @author Marvin Schütz
+     */
+    @Test
+    public void testeIsBlankTrue() {
+        assertTrue("Null muss als String mit Blanks anerkannt werden", StringUtils.isBlank(null));
+        assertTrue("Der String Besteht nur aus Blanks", StringUtils.isBlank(" "));
+        assertTrue("Der String Besteht nur aus Blanks", StringUtils.isBlank("          "));
+    }
+    /**
+     * tetstet die Methode auf false Ausgabe.
+     * @author Marvin Schütz
+     */
+    @Test
+    public void testeIsBlankFalse() {
+        assertFalse("String enhält zeichen es sollte false zurückgegeben werden", StringUtils.isBlank("hsdghdfk   sdfg"));
+        assertFalse("String enhält zeichen es sollte false zurückgegeben werden", StringUtils.isBlank("   hsdghdfkfg"));
+        assertFalse("String enhält zeichen es sollte false zurückgegeben werden", StringUtils.isBlank("&%!?"));
+    }
+    /**
+     * Testet die Methode join auf exception.
+     * @author Marvin Schütz
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testeJoinIllegalArgumentexception() {
+        StringUtils.join();
+    }
+    /**
+     * Teste die Methode join auf richtige Ausgabe.
+     * @author Marvin Schütz
+     */
+    @Test
+    public void testeJoinAusgabe(){
+        assertEquals("falscher String","hallo",StringUtils.join("hallo"));
+        assertEquals("falscher String","hallo,du",StringUtils.join("hallo","du"));
+        assertEquals("falscher String","hallo,(null)",StringUtils.join("hallo",null));
+        assertEquals("falscher String","hallo,(null),du",StringUtils.join("hallo",null,"du"));
+    }
+    /**
+     * Testet die Methode isValidIsbn auf false Rückgabe.
+     * @author Marvin Schütz
+     */
+    @Test
+    public void testeIsValidIsbnFalse(){
+        assertFalse("EIngabe null muss false ergeben",StringUtils.isValidIsbn10(null));
+        assertFalse("leerer string muss false ergeben",StringUtils.isValidIsbn10(""));
+        assertFalse("zu langer string muss false ergeben",StringUtils.isValidIsbn10("1235468476465489456134823"));
+        assertFalse("zu viele formatierungszeichen muss false ergeben",StringUtils.isValidIsbn10("3--1258--9825"));
+        assertFalse("zu viele formatierungszeichen muss false ergeben",StringUtils.isValidIsbn10("3  1258  9825"));
+        assertFalse("Buchstaben sind nicht erlaubt",StringUtils.isValidIsbn10("3-8fg21-453-9"));
+        assertFalse("Ungültige Prüfziffer",StringUtils.isValidIsbn10("3-89721-453-5"));
+        assertFalse("Sonderzeichen sind nicht erlaubt",StringUtils.isValidIsbn10("3-8!?21-453-9"));
+    }
+    /**
+     * Testet die Methode isValidIsbn auf true Rückgabe-
+     * @author Marvin Schütz
+     */
+    @Test
+    public void testeIsValidIsbnTrue(){
+        assertTrue("Die ISBN ist gültig",StringUtils.isValidIsbn10("3-89721-453-9"));
+    }
 }
 
