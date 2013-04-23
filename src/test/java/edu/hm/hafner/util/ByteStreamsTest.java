ece5fc2e2cf45dd6623c6cf6e4dafb39e7042ad7
Marvin Schuetz <marvin-spiky@gmx.net>
2013-04-23 15:52:34 +0200

    Neue Testklasse ByteStreamsTest

---
+++ src/test/java/edu/hm/hafner/util/ByteStreamsTest.java
@@ -0,0 +1,14 @@
+package edu.hm.hafner.util;
+
+/**
+ * FIXME: Document type ByteStreamsTest.
+ *
+ * @author Marvin Schütz
+ * @author Sebastian Hansbauer
+ */
+public class ByteStreamsTest {
+    /**
+     *
+     */
+}
+
