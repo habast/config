804389b8b8742e2645c2ac97bad1d12f649802b1
Marvin Schuetz <marvin-spiky@gmx.net>
2013-04-28 14:41:09 +0200

    befor merge

--- src/test/java/edu/hm/hafner/util/ByteStreamsTest.java
+++ src/test/java/edu/hm/hafner/util/ByteStreamsTest.java
@@ -1,14 +1,40 @@
 package edu.hm.hafner.util;
 
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.google.common.io.ByteStreams;
+import com.google.common.io.OutputSupplier;
+
+import org.junit.Test;
 /**
- * FIXME: Document type ByteStreamsTest.
- *
+ * Testet die ByteStream.copy() Methode.
  * @author Marvin Schütz
  * @author Sebastian Hansbauer
  */
 public class ByteStreamsTest {
     /**
-     *
+     *Testet die Methode ByteStream.copy.
+     *@author Marvin Schütz
+     *@author Sebastian Hansbauer
+     * @throws IOException
      */
+    @Test
+    public void byteStreamCopyTest() throws IOException{
+        InputStream from = mock(InputStream.class);
+        when(from.read()).thenReturn(-1);
+
+        OutputStream out = mock(OutputStream.class);
+
+        OutputSupplier<OutputStream> outs = mock(OutputSupplier.class);
+        when(outs.getOutput()).thenReturn(out);
+
+       ByteStreams.copy(from,outs);
+      // assertEquals(1,ByteStreams.copy(in,outs));
+        verify(from).read();
+    }
 }
 
